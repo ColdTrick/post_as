ea7d39d3db07bbdc52a46d9f840e6dafa8515075
Jerome Bakker <jeabakker@coldtrick.com>
2018-06-25 15:05:59 +0200

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.1
+ * On 2018-06-25 15:04
+ */
+
+return array (
+  'post_as:usersettings:description' => 'Hieronder kun je andere gebruiker van de site autoriseren om namens jou content te kunnen publiceren.
+Voeg alleen gebruikers toe welke je ook vertrouwd.',
+  'post_as:usersettings:authorized_users' => 'Geautoriseerde gebruikers welke kunnen publiceren namens jou',
+  'post_as:input:label' => 'Publiceer namens',
+  'post_as:myself' => 'Mijzelf (%s)',
+);
